e08d4e3a3b036ab7239e558accee3bde74507b6d
gyk <3490315473@qq.com>
2022-10-30 15:59:20 +0800

    hot-fix commit1

--- src/main/java/edu/hncj/test.java
+++ src/main/java/edu/hncj/test.java
@@ -4,5 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2 !");
+        System.out.println("hello hot-fix!");
+        System.out.println("hello hot-fix!");
     }
 }
