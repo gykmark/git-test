5e14b390817d8273c5f6acbefb458e2dc016320e
gyk <3490315473@qq.com>
2022-10-30 20:55:08 +0800

    master1 commit4

--- src/main/java/edu/hncj/test.java
+++ src/main/java/edu/hncj/test.java
@@ -10,5 +10,6 @@ public class test {
         System.out.println("hello hot-fix3!");
         System.out.println("hello pull!");
         System.out.println("hello push!");
+        System.out.println("hello push!");
     }
 }
